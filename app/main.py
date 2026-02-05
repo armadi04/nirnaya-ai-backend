2ac316f6aa6b9f3e931ca52f8063f238ea770919
Armadi Nurhidayat <armadinurhidayat04@gmail.com>
2026-02-05 21:24:56 +0700

    edit port

--- app/main.py
+++ app/main.py
@@ -109,10 +109,14 @@ async def health_check():
 
 
 if __name__ == "__main__":
+    import os
     import uvicorn
+    
+    port = int(os.environ.get("PORT", 8000))
+    
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8000,
+        port=port,
         reload=settings.debug
     )
